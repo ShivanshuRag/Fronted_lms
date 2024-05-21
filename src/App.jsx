29d49ddec3a9d9a962635639068bbe8dedb06a49
ShivanshuRag <ragsingh@773gmail.com>
2024-05-21 09:31:28 +0530

    fronted setup with react Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <>
+   <h1
+   className="text-3xl font-bold underline"> Hello  I am Shiavanshu SinghRag</h1>
+   </>
+  )
+}
+
+export default App
+ 
