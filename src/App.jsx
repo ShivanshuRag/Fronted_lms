a273c7b0ad43ed4165b76e006bb468d6063e048a
ShivanshuRag <ragsingh@773gmail.com>
2024-05-25 09:13:00 +0530

    configure regexMatcher and signup page and fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Route,Routes  } from 'react-router-dom'
 import AboutUs from './Pages/AboutUsPage.jsx'
 import HomePage from './Pages/HomePage.jsx'
 import NotFound from './Pages/NotFound.jsx'
+import Signup from './Pages/Signup.jsx'
 
 function App() {
  
@@ -18,6 +19,8 @@ function App() {
     <Routes>
     <Route path="/" element={<HomePage />} ></Route>
     <Route path="/about" element={<AboutUs/>} ></Route>
+    <Route path="/signup" element={<Signup/>} ></Route>
+
     <Route path="*" element={<NotFound/>}></Route>
     </Routes>
     
