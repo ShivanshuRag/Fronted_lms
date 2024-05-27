3c5e2ec97bafecb30adbdf615b68144f04aaf988
ShivanshuRag <ragsingh@773gmail.com>
2024-05-27 11:05:17 +0530

    configure create new course functionalty and fix syntax error

--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -5,6 +5,67 @@ import { createAsyncThunk , createSlice } from "@reduxjs/toolkit";
 
  import axiosInstance from "../../Helpers/axiosIntances.js";
 
- const intialState = {
+ const initialState = {
     courseData: []
  }
+
+ export const getAllCourses = createAsyncThunk("/course/get" , async()=>{
+   try {
+
+     const response = axiosInstance.get("/courses")
+        toast.promise( response , {
+        loading: "Loading Course Data...",
+        success: "Courses Loaded successfully",
+         error:  " Failed to get courses"
+     })
+     return (await response).data.courses
+   } catch (error) {
+      toast.error(error?.response?.data?.message)
+   }
+
+ });
+
+  export const createNewCourse  = createAsyncThunk("course/create" , async (data)=>{
+     try {
+       let formData = new FormData();
+        formData.append("title" , data?.title);
+        formData.append("description" , data?.description);
+        formData.append("category" , data?.category);
+        formData.append("createdBy" , data?.createdBy);
+        formData.append("thumbnail" , data?.thumbnail);
+
+        const response = axiosInstance.post("courses" , formData)
+         toast.promise(response , {
+           loading : "created new course..",
+           success : "course created successfully",
+           error : " Failed to create course"
+
+         })
+
+         return (await response).data ;
+
+     } catch (error) {
+        toast.error(error?.response?.data?.message);
+     }
+
+
+  })
+
+
+
+ const courseSlice = createSlice({
+    name : "courses",
+    initialState,
+    reducers: {},
+    extraReducers: (builder)=>{
+      builder.addCase(getAllCourses.fulfilled, (state , action)=>{
+
+        if( action.payload){
+            state.courseData = [...action.payload];
+        }
+      })
+    }
+ 
+ });
+
+ export default courseSlice.reducer
