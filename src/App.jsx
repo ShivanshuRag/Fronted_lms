bca70ee77ebd92ad26c1022dcb4a4a919f613109
ShivanshuRag <ragsingh@773gmail.com>
2024-05-28 18:36:56 +0530

    configure create course file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AboutUs from './Pages/AboutUsPage.jsx'
 import Contact from './Pages/Contact.jsx'
 import CourseDescription from './Pages/Course/CourseDescription.jsx'
 import CourseList from './Pages/Course/CourseList.jsx'
+import CreateCourse from './Pages/Course/CreateCourse.jsx'
 import Denied from './Pages/Denied.jsx'
 import HomePage from './Pages/HomePage.jsx'
 import Login from './Pages/Login.jsx'
@@ -28,6 +29,7 @@ function App() {
     <Route path="/course/description" element={<CourseDescription/>}></Route>
     <Route path="/contact" element={<Contact/>}></Route>
     <Route path="/denied" element={<Denied/>}></Route>
+    <Route path="/course/create" element={<CreateCourse/>}></Route>
     <Route path="*" element={<NotFound/>}></Route>
     </Routes>
     
