f8c0b22025cb20bcf5775cf5f8909154523c0d8a
ShivanshuRag <ragsingh@773gmail.com>
2024-06-08 11:14:57 +0530

    configure razorpay Slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,10 +3,12 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import authSliceReducer from './Slices/AuthSlice.js';
 import courseSliceReducer from './Slices/CourseSlice.js'
+import RazorpaySliceReducer from './Slices/RazorpaySlice.js';
 const store = configureStore({
     reducer: {
       auth : authSliceReducer,
       course :courseSliceReducer,
+      razorpay : RazorpaySliceReducer,
     },
     devTools : true
 });
