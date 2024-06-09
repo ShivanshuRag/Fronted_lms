91d1411b979ff612840e2da8f206961bd7fbfad3
ShivanshuRag <ragsingh@773gmail.com>
2024-06-09 18:20:02 +0530

    create checkout failure page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import HomePage from './Pages/HomePage.jsx'
 import Login from './Pages/Login.jsx'
 import NotFound from './Pages/NotFound.jsx'
 import Checkout from './Pages/Payment/Checkout.jsx'
+import CheckoutFailure from './Pages/Payment/CheckoutFailure.jsx'
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess.jsx'
 import Signup from './Pages/Signup.jsx' 
 import EditProfile from './Pages/User/EditProfile.jsx'
@@ -45,6 +46,7 @@ function App() {
       <Route path="/user/editprofile" element={<EditProfile/>}/>
       <Route path="/checkout" element={<Checkout/>}/> 
       <Route path="/checkout/success" element={<CheckoutSuccess/>}/>
+      <Route path="/checkout/fail" element={<CheckoutFailure/>}/>
     </Route>
    
     <Route path="*" element={<NotFound/>}></Route>
--- src/Pages/Payment/CheckoutSuccess.jsx
+++ src/Pages/Payment/CheckoutSuccess.jsx
@@ -19,7 +19,7 @@ function CheckoutSuccess(){
  return(
     <HomeLayout>
       <div className="min-h-[90vh] flex items-center justify-center text-white">
-        <div className="w-80 h-[20rem] flex flex-col justify-center items-center shadow-[0_0_10px_black] rounded-lg relative">
+        <div className="w-80 h-[26rem] flex flex-col justify-center items-center shadow-[0_0_10px_black] rounded-lg relative">
           <h1 className="bg-green-500 absolute text-center top-0 w-full py-2 text-2xl font-bold rounded-tl-lg rounded-tr-lg">Payment Successfull</h1> 
              <div className="px-4 flex flex-col items-center justify-center space-y-2">
                 <div className="text-center space-y-2">
