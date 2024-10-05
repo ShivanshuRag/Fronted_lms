ab8f522979738e5a428657b9e186eece7097a611
ShivanshuRag <ragsingh773@gmail.com>
2024-10-05 10:46:32 +0530

    add proxy and server url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy:{
+      '/api/v1' : 'https://persona-bknd.vercel.app'
+    }
+  },
+
   plugins: [react()],
 })
